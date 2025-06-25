25f27739fee1185e165f22cf40b054bcebeb8738
Zari <zjcmagnaye@gmail.com>
2025-06-24 20:27:06 -0700

    Main Function

--- todo.py
+++ todo.py
@@ -2,6 +2,7 @@ tasks = []
 
 # Function to display options
 def display_menu():
+    print("TO-DO LIST MENU")
     print("1. Add tasks")
     print("2. View tasks")
     print("3. Delete tasks")
@@ -13,3 +14,23 @@ def add_task():
     tasks.append(new_task)
     print("Task {new_task} added succesfully!")
     print(tasks)
+
+
+
+# Main Function
+def main():
+    while True:
+        display_menu()
+        choice = input("What would you like to do? ")
+        if choice == "1":
+            add_task()
+        elif choice == "2":
+            view_task()
+        elif choice == "3":
+            delete_task()    
+        elif choice == "4":
+            break()
+        else:
+            print("Invalid choice. Please try again.")
+
+main()
